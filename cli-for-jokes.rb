a836cd8ea0ce2566d34bef99076dba84dc5766f7
helen_harman <hlharman@bellsouth.net>
2021-06-28 16:41:45 -0400

    first commit after initial

---
+++ cli-for-jokes.rb
@@ -0,0 +1,18 @@
+require 'bundler'
+require_all 'lib'
+require 'pry'
+require 'project using jokes api and ruby.txt'
+
+
+puts "Hello, would you like a joke?"
+accept answer
+choose type of joke, etc.
+gets joke
+
+
+
+
+A short description
+Install instructions
+A contributor's guide
+A link to the license for your code
---
+++ jokes-with-api.rb
@@ -0,0 +1,58 @@
+require 'bundler'
+
+require_all 'lib'
+require 'pry'
+
+class Joke
+Joke.new 
+
+ https://v2.jokeapi.dev/joke/[Category/-ies]
+This endpoint is the one you want to call to get a joke.
+
+
+A valid joke category or multiple joke categories, delimited with a comma, plus or minus character (, + -) must be provided in the URL.
+These are all the available categories: Any, Misc, Programming, Dark, Pun, Spooky, Christmas
+
+
+There are six different filtering methods in JokeAPI:
+Joke Category
+Blacklist Flags
+Response Format
+Joke Type
+Search String
+ID Range
+Language
+
+
+Supported URL parameters:
+?format
+?blacklistFlags
+?lang
+?idRange
+?contains
+?type
+?amount
+
+
+def initialize
+#to set all variables associated with choosing a joke back to default
+end 
+
+X-RapidAPI-Key
+website - "api-info": "JokeAPI v2.3.1 (https://v2.jokeapi.dev)",
+def gets 
+https://v2.jokeapi.dev/
+end 
+
+
+def print 
+puts joke
+end 
+
+def error handling 
+end 
+
+
+end 
+
+
